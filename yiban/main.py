4c079db4285cc3f658138101957ed68431e99ff7
ilotus <jilotus@qq.com>
2022-03-28 20:17:58 +0800

    modify readme

--- yiban/main.py
+++ yiban/main.py
@@ -6,6 +6,8 @@ from names import names
 
 if __name__ == '__main__':
     for name in names:
+        if name['userid'] == "" or name['pwd'] == "":
+            break
         yiban_daka(username=name['userid'],
                    password=name['pwd'],)
         time.sleep(1)
